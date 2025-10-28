3d6494a21790a8beee6ae5b99b9a4debab1390a7
WnRock <141114704+WnRock@users.noreply.github.com>
2025-10-28 18:44:03 +0800

    chore: migrate to dynamic configuration

---
+++ app.config.js
@@ -0,0 +1,68 @@
+const CHECKLIST_BUILD_VERSION = process.env.CHECKLIST_BUILD_VERSION || "0.0.0";
+const CHECKLIST_BUILD_ARCH = process.env.CHECKLIST_BUILD_ARCH || "universal";
+
+const buildVersion = CHECKLIST_BUILD_VERSION;
+const buildArch =
+  CHECKLIST_BUILD_ARCH !== "universal"
+    ? [CHECKLIST_BUILD_ARCH]
+    : ["armeabi-v7a", "arm64-v8a", "x86", "x86_64"];
+
+module.exports = {
+  expo: {
+    name: "Checklist",
+    slug: "Checklist",
+    version: buildVersion,
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "checklist",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "dev.wnrock.checklist",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      package: "dev.wnrock.checklist",
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          android: {
+            buildArchs: buildArch,
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "b663614a-67f1-4ee5-8ddd-457898a005e8",
+      },
+    },
+    owner: "wnrock",
+  },
+};
